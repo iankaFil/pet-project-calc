a99b6b7ad1900621ae709c3e5fe5d5369c49f870
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-01-25 20:02:26 -0700

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import { Box, Text } from '@chakra-ui/react';
+import Numbers from './Numbers/Numbers';
+import { useState } from 'react';
+
 export const App = () => {
+  const [counts, setCounts] = useState('0');
+  const [result, setResult] = useState('');
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        h="100vh"
+      >
+        <Box
+          display="flex"
+          gap="5px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          w="200vh"
+        >
+          <Box display="flex" w="100%" justifyContent="between">
+            <Text
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              bg="gray.50"
+              w="100%"
+              h="38px"
+              px="4px"
+              borderRadius="8px"
+            >
+              {counts}
+            </Text>
+            <Text w="fit-content" h="38px" textColor="orange">
+              {result}
+            </Text>
+          </Box>
+          <Numbers data={counts} onClick={setCounts}></Numbers>
+        </Box>
+      </Box>
     </div>
   );
 };
---
+++ src/components/Numbers/Numbers.jsx
@@ -0,0 +1,29 @@
+import { Box, Button } from '@chakra-ui/react';
+
+const Numbers = props => {
+  const nums = Array.from(Array(10).keys()).map(number => {
+    return (
+      <Button
+        onClick={e => {
+          if (props.data !== 0) {
+            props.onClick(props.data + e.target.innerHTML);
+          } else {
+            props.onClick(e.target.innerHTML);
+          }
+        }}
+        key={number}
+        w="40px"
+        h="40px"
+        margin="4px"
+      >
+        {number}
+      </Button>
+    );
+  });
+  return (
+    <Box display="flex" flexWrap="wrap" w="150px">
+      {nums}
+    </Box>
+  );
+};
+export default Numbers;
